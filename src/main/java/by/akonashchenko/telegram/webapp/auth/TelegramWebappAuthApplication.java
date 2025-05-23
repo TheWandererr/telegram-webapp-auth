31bae14776559ce2571b00b8fd4f63caa4d16c46
TheWandererr <kartem2013@yandex.by>
2025-05-23 22:09:29 +0300

    init

---
+++ src/main/java/by/akonashchenko/telegram/webapp/auth/TelegramWebappAuthApplication.java
@@ -0,0 +1,13 @@
+package by.akonashchenko.telegram.webapp.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramWebappAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramWebappAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/akonashchenko/telegram/webapp/auth/TelegramWebappAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package by.akonashchenko.telegram.webapp.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramWebappAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
